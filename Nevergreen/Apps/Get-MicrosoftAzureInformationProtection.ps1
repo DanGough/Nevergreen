afd95e6ac61656675d636c7e359ad4fe9c4e08cf
Dan Gough <goughdj@gmail.com>
2021-05-14 20:53:17 +0100

    Tidy inconsistent formatting and variable names

--- Nevergreen/Apps/Get-MicrosoftAzureInformationProtection.ps1
+++ Nevergreen/Apps/Get-MicrosoftAzureInformationProtection.ps1
@@ -1,9 +1,10 @@
 $Version = ((Invoke-WebRequest 'https://docs.microsoft.com/en-us/azure/information-protection/rms-client/unifiedlabelingclient-version-release-history' -UseBasicParsing).Content | Select-String -Pattern '>Version\s(.+)</h2>').Matches.Groups[1].Value
-$URL = ((Invoke-WebRequest 'https://www.microsoft.com/en-us/download/confirmation.aspx?id=53018' -UseBasicParsing).Links | Where-Object href -like '*_UL.msi')[0].href
+$URL32 = ((Invoke-WebRequest 'https://www.microsoft.com/en-us/download/confirmation.aspx?id=53018' -UseBasicParsing).Links | Where-Object href -Like '*_UL.msi')[0].href
 
 if ($Version -and $URL) {
   [PSCustomObject]@{
     Version      = $Version
-    URI          = $URL
+    Architecture = 'x86'
+    URI          = $URL32
   }
 }
--- Nevergreen/Apps/Get-RadioDetectionCatManager.ps1
+++ Nevergreen/Apps/Get-RadioDetectionCatManager.ps1
@@ -1,12 +1,12 @@
 $Resp = Invoke-WebRequest -Uri 'https://www.radiodetection.com/en-gb/resources/software-downloads/cat4-manager' -UseBasicParsing
 
 $Version = ($Resp.Content | Select-String -Pattern 'Version:\s(.+)<').Matches.Groups[1].Value
-$URL = $Resp.Links | Where-Object href -Like '*.zip*' | Select-Object -First 1 -ExpandProperty href
+$URL32 = $Resp.Links | Where-Object href -Like '*.zip*' | Select-Object -First 1 -ExpandProperty href
 
-if ($Version -and $URL) {
+if ($Version -and $URL32) {
     [PSCustomObject]@{
         Version      = $Version
         Architecture = 'x86'
-        URI          = $URL
+        URI          = $URL32
     }
 }
