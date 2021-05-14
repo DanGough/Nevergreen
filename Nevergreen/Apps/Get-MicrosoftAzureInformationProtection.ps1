b9146828bbab0cfc5eed595bdfef6c2de80cbed8
Dan Gough <goughdj@gmail.com>
2021-05-14 22:23:47 +0100

    Standardised version RegEx: ((?:\d+\.)+(?:\d+))

--- Nevergreen/Apps/Get-MicrosoftAzureInformationProtection.ps1
+++ Nevergreen/Apps/Get-MicrosoftAzureInformationProtection.ps1
@@ -1,7 +1,7 @@
-$Version = ((Invoke-WebRequest 'https://docs.microsoft.com/en-us/azure/information-protection/rms-client/unifiedlabelingclient-version-release-history' -UseBasicParsing).Content | Select-String -Pattern '>Version\s(.+)</h2>').Matches.Groups[1].Value
+$Version = ((Invoke-WebRequest 'https://docs.microsoft.com/en-us/azure/information-protection/rms-client/unifiedlabelingclient-version-release-history' -UseBasicParsing).Content | Select-String -Pattern '>Version\s((?:\d+\.)+(?:\d+))</h2>').Matches.Groups[1].Value
 $URL32 = ((Invoke-WebRequest 'https://www.microsoft.com/en-us/download/confirmation.aspx?id=53018' -UseBasicParsing).Links | Where-Object href -Like '*_UL.msi')[0].href
 
-if ($Version -and $URL) {
+if ($Version -and $URL32) {
   [PSCustomObject]@{
     Version      = $Version
     Architecture = 'x86'
