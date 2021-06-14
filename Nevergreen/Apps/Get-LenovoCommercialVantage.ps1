8d070402ae2b693455fdcb9d074c1ca082223a1f
Dan Gough <goughdj@gmail.com>
2021-06-14 20:43:39 +0100

    Add Lenovo Commercial Vantage

--- Nevergreen/Apps/Get-LenovoCommercialVantage.ps1
+++ Nevergreen/Apps/Get-LenovoCommercialVantage.ps1
@@ -1,9 +1,8 @@
-$URL32 = ((Invoke-WebRequest 'https://support.lenovo.com/gb/en/solutions/hf003321-lenovo-vantage-for-enterprise' -UseBasicParsing).Content | Select-String -Pattern '.+(https:\/\/((?:d.+\.)+(?:\d.+)).zip)').Matches.Groups[1].Value
-$Version = ($URL32 | Select-String -Pattern 'LenovoCommercialVantage_((?:\d+\.)+(?:\d.+)).zip').Matches.Groups[1].Value
+$URL32 = ((Invoke-WebRequest 'https://support.lenovo.com/gb/en/solutions/hf003321-lenovo-vantage-for-enterprise' -UseBasicParsing).Content | Select-String -Pattern '.+(https://.+LenovoCommercialVantage_(?:\d+\.)+\d+.+zip)').Matches.Groups[1].Value
 
-if ($Version -and $URL32) {
+if ($URL32 -match '((?:\d+\.)+\d+)') {
     [PSCustomObject]@{
-        Version      = $Version
+        Version      = $matches[1]
         Architecture = 'x86'
         URI          = $URL32
     }
