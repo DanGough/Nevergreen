4776a55328fa7a706688ee3573ef8ac5df4de7de
Dan Gough <goughdj@gmail.com>
2021-06-14 20:00:46 +0100

    Add Get-8x8Work

--- Nevergreen/Apps/Get-8x8Work.ps1
+++ Nevergreen/Apps/Get-8x8Work.ps1
@@ -1,7 +1,10 @@
-$URL32= ((Invoke-WebRequest 'https://support.8x8.com/cloud-phone-service/voice/work-desktop/download-8x8-work-for-desktop' -UseBasicParsing).Links | Where-Object href -Like '*work-32-msi*.msi')[0].href
-$URL64 = ((Invoke-WebRequest 'https://support.8x8.com/cloud-phone-service/voice/work-desktop/download-8x8-work-for-desktop' -UseBasicParsing).Links | Where-Object href -Like '*work-64-msi*.msi')[0].href
-$Version32 = ($URL32 | Select-String -Pattern 'work-32-msi-v((?:\d+\.)+(?:\d.+)).msi').Matches.Groups[1].Value
-$Version64 = ($URL64 | Select-String -Pattern 'work-64-msi-v((?:\d+\.)+(?:\d.+)).msi').Matches.Groups[1].Value
+$Response = Invoke-WebRequest 'https://support.8x8.com/cloud-phone-service/voice/work-desktop/download-8x8-work-for-desktop' -UseBasicParsing
+
+$URL32 = $Response.Links | Where-Object href -Like '*work-32-msi*.msi' | Select-Object -Expand href -First 1
+$URL64 = $Response.Links | Where-Object href -Like '*work-64-msi*.msi' | Select-Object -Expand href -First 1
+
+$Version32 = $URL32 -replace '.+work-32-msi-v((?:\d+\.)+\d+(?:-\d+)?).msi','$1' -replace '-','.'
+$Version64 = $URL64 -replace '.+work-64-msi-v((?:\d+\.)+\d+(?:-\d+)?).msi','$1' -replace '-','.'
 
 if ($Version32 -and $URL32) {
     [PSCustomObject]@{
