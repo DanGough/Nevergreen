310422b444f766ca10929167d6bcbb6559decb88
Dan Gough <goughdj@gmail.com>
2021-06-14 20:25:29 +0100

    Update URL

--- Nevergreen/Apps/Get-AutoIt.ps1
+++ Nevergreen/Apps/Get-AutoIt.ps1
@@ -1,6 +1,10 @@
 $Response = Invoke-WebRequest 'https://www.autoitscript.com/site/autoit/downloads' -UseBasicParsing
 
-$URL32 = 'https://www.autoitscript.com' + ($Response.Links | Where-Object href -like '*autoit-v*-setup.exe' | Select-Object -ExpandProperty href -First 1)
+$URL32 = $Response.Links | Where-Object href -like '*autoit-v*-setup.exe' | Select-Object -ExpandProperty href -First 1
+
+if ($URL32 -match '.exe$' -and $URL32 -notmatch '^http') {
+    $URL32 = 'https://www.autoitscript.com' + $URL32
+}
 
 if ($URL32 -and $Response.Content -match 'v((?:\d+\.)+\d+)') {
     [PSCustomObject]@{
