15de858e15bce1fbf0d27c3ef5bf419e82823516
Dan Gough <goughdj@gmail.com>
2021-06-15 22:06:13 +0100

    Add Get-CiscoWebexSupportManager

--- Nevergreen/Apps/Get-CiscoWebexSupportManager.ps1
+++ Nevergreen/Apps/Get-CiscoWebexSupportManager.ps1
@@ -1,5 +1,6 @@
-$Version = ((Invoke-WebRequest 'https://welcome.webex.com/sc3300/supportcenter/site/siteabout.do?siteurl=welcome' -UseBasicParsing).Content | Select-String -Pattern '((?:\d+\.)+(?:\d+))' -AllMatches).Matches[1].Groups[0].Value
-$URL32 = ((Invoke-WebRequest 'https://welcome.webex.com/sc3300/supportcenter/site/downloads.do?siteurl=welcome' -UseBasicParsing).Links | Where-Object href -Like '*webexsc.msi')[0].href
+$Version = $null
+$Version = ((Invoke-WebRequest 'https://welcome.webex.com/sc3300/supportcenter/site/siteabout.do?siteurl=welcome' -DisableKeepAlive -UseBasicParsing).Content | Select-String -Pattern '((?:\d+\.)+(?:\d+))' -AllMatches).Matches[1].Groups[0].Value
+$URL32 = (Invoke-WebRequest 'https://welcome.webex.com/sc3300/supportcenter/site/downloads.do?siteurl=welcome' -DisableKeepAlive -UseBasicParsing).Links | Where-Object href -Like '*webexsc.msi' | Select-Object -ExpandProperty href -First 1
 
 if ($Version -and $URL32) {
     [PSCustomObject]@{
