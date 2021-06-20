1cce824ca37d6ed45627558a305fd919df8ab016
Dan Gough <goughdj@gmail.com>
2021-06-20 12:55:21 +0100

    Removed version from URL so it defaults to latest

--- Nevergreen/Apps/Get-MicrosoftSSMS.ps1
+++ Nevergreen/Apps/Get-MicrosoftSSMS.ps1
@@ -1,5 +1,5 @@
 $URL64 = 'https://aka.ms/ssmsfullsetup'
 
-$Version = (Invoke-WebRequest 'https://docs.microsoft.com/en-us/sql/ssms/download-sql-server-management-studio-ssms?view=sql-server-ver15' -UseBasicParsing).Content | Get-Version -Pattern 'Release number: ((?:\d+\.)+\d+)'
+$Version = (Invoke-WebRequest 'https://docs.microsoft.com/en-us/sql/ssms/download-sql-server-management-studio-ssms' -UseBasicParsing).Content | Get-Version -Pattern 'Release number: ((?:\d+\.)+\d+)'
 
 New-NevergreenApp -Version $Version -Uri $URL64 -Architecture 'x64'
