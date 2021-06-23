d2be6d4f121b9adf0f93069f4b1a32d7134f0bb9
Dan Gough <goughdj@gmail.com>
2021-06-23 22:05:58 +0100

    Fix Get-AdobeCreativeCloud

--- Nevergreen/Apps/Get-AdobeCreativeCloud.ps1
+++ Nevergreen/Apps/Get-AdobeCreativeCloud.ps1
@@ -1,6 +1,6 @@
 $URL32,$URL64,$URLARM64 = Get-Link -Uri 'https://helpx.adobe.com/ca/download-install/kb/creative-cloud-desktop-app-download.html' -MatchProperty href -Pattern 'win32','win64','winarm64'
-$Version32,$Version64,$VersionARM64 = $URL64,$URL32,$URLARM64 | Get-Version -Pattern 'ACCCx((?:\d+_)+\d+)\.zip$' -ReplaceWithDot
+$Version32,$Version64,$VersionARM64 = $URL32,$URL64,$URLARM64 | Get-Version -Pattern 'ACCCx((?:\d+_)+\d+)\.zip$' -ReplaceWithDot
 
 New-NevergreenApp -Version $Version32 -Uri $URL32 -Architecture 'x86'
-New-NevergreenApp -Version $Version64 -Uri $URL32 -Architecture 'x64'
+New-NevergreenApp -Version $Version64 -Uri $URL64 -Architecture 'x64'
 New-NevergreenApp -Version $VersionARM64 -Uri $URLARM64 -Architecture 'ARM64'
