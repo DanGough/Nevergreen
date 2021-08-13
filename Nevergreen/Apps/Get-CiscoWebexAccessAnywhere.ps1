e3562d38283a5f07dadf51fcf1ca21c337405b83
Ashley How <ashleyhow@gmail.com>
2021-08-13 17:24:12 +0100

    Add Cisco Webex Access Anywhere
    There doesn't seem to be any place I could find publicly to scrape the URL without account authentication (with authentication to our webex support portal I can manage to pull https://akamaicdn.webex.com/client/WBXclient-41.8.4-11/aainstall.msi but that's a no go for obvious reasons) so I've had to settle for a static URL instead. This uses the same source location (https://welcome.webex.com/client/T33L/...) which comes back via an API call I found for Cisco Webex app (which Arron integrated into Nevergreen) but annoyingly it doesn't include Webex Access Anywhere app!
    
    Versioning is pulled from the same source that is used by Webex Support Manager - the public webex support portal as they always seem in sync.

---
+++ Nevergreen/Apps/Get-CiscoWebexAccessAnywhere.ps1
@@ -0,0 +1,13 @@
+$RetryCount = 3
+
+for ($i = 0; $i -lt $RetryCount; $i++) {
+    try {
+        $Version = ((Invoke-WebRequest 'https://welcome.webex.com/sc3300/supportcenter/site/siteabout.do?siteurl=welcome' -DisableKeepAlive -UseBasicParsing -ErrorAction Stop).Content | Select-String -Pattern '(?:\d+\.)+(?:\d+)' -AllMatches).Matches.Groups.Value | Select-Object -Skip 1 -First 1
+        $URL32 = "https://welcome.webex.com/client/T33L/aainstall.msi"
+        New-NevergreenApp -Name 'Cisco Webex Access Anyhwere' -Version $Version -Uri $URL32 -Architecture 'x86' -Type 'Msi'
+        break
+    }
+    catch {
+        Write-Warning "Attempt $($i+1) failed, retrying..."
+    }
+}
