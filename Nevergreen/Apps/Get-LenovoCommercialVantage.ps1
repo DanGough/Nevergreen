656df1f7adc075bfb0accd54f0df7ca47c8eb803
Dan Gough <goughdj@gmail.com>
2021-09-12 21:22:46 +0100

    Update to use js file as reference

--- Nevergreen/Apps/Get-LenovoCommercialVantage.ps1
+++ Nevergreen/Apps/Get-LenovoCommercialVantage.ps1
@@ -1,5 +1,8 @@
-$URL32 = Get-Version -Uri 'https://support.lenovo.com/gb/en/solutions/hf003321-lenovo-vantage-for-enterprise' -Pattern '.+(https://.+LenovoCommercialVantage_(?:\d+\.)+\d+.+zip)'
+# js file referenced in https://support.lenovo.com/gb/en/solutions/hf003321-lenovo-vantage-for-enterprise; hopefully stays the same for future versions, will require monitoring
+# Abusing Get-Version to return URL as it can extract regex patterns from web content
 
-$Version = $URL32 | Get-Version
+$Version = Get-Version -Uri 'https://support.lenovo.com/gb/en/api/v4/contents/cdn/hf003321_1626445331000.js' -Pattern 'Version ((?:\d+\.)+\d+)'
+
+$URL32 = Get-Version -Uri 'https://support.lenovo.com/gb/en/api/v4/contents/cdn/hf003321_1626445331000.js' -Pattern 'href=\\"(https://.+LenovoCommercialVantage.+zip)'
 
 New-NevergreenApp -Name 'Lenovo Commercial Vantage' -Version $Version -Uri $URL32 -Architecture 'x86' -Type 'Zip'
