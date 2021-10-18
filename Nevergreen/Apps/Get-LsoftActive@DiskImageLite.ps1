f31a62e97822106201f53e658eeaa056652f531b
Dan Gough <goughdj@gmail.com>
2021-10-18 21:10:27 +0100

    Add LSoft Active Disk Image Lite

--- Nevergreen/Apps/Get-LsoftActive@DiskImageLite.ps1
+++
@@ -1,4 +0,0 @@
-$URL = Get-Link -Uri 'https://www.disk-image.com/download.htm' -MatchProperty href -Pattern '\DiskImageFreeSetup.exe$' -PrefixDomain
-$Version = ((Invoke-WebRequest 'https://www.disk-image.com/history.htm' -DisableKeepAlive -UseBasicParsing).Content | Select-String -Pattern '(?:\d+\.)+(?:\d+)' -AllMatches).Matches.Groups.Value | Sort-Object { [version]$_ } -Descending | Select-Object -First 1
-
-New-NevergreenApp -Name 'LSoft Active@ Disk Image Lite' -Version $Version -Uri $URL -Architecture 'x86' -Type 'Exe'
