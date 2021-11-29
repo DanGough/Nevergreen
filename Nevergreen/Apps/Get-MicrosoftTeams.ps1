52889a1f51596b4edb850c7a73e4bf2f11a49daa
Jonathan Pitre <jakeld@gmail.com>
2021-11-29 17:30:06 -0500

    Add Teams Web View 2 versions Fix Regex for latest production version (now called public)

--- Nevergreen/Apps/Get-MicrosoftTeams.ps1
+++ Nevergreen/Apps/Get-MicrosoftTeams.ps1
@@ -1,21 +1,26 @@
-$DownloadURL = 'https://raw.githubusercontent.com/ItzLevvie/MicrosoftTeams-msinternal/master/defconfig'
+$DownloadURL = 'https://raw.githubusercontent.com/ItzLevvie/MicrosoftTeams-msinternal/master/defconfig2'
 $DownloadText = (Invoke-WebRequest -Uri $DownloadURL -DisableKeepAlive -UseBasicParsing).Content
 
-$Rings = @('Continuous Deployment', 'Exploration', 'Preview', 'Production')
+$Rings = @('Canary', 'Teams', 'IT Admins Preview', 'Developers Partners Preview', 'Microsoft Dogfood', 'TAP/Beta Users', 'Public Developer Preview', 'Microsoft Outer Ring', 'Public')
 $Architectures = @('x64', 'x86', 'ARM64')
-$Types = @('Exe', 'MSI')
+$Types = @('Exe', 'Msi', 'Msix')
 
-foreach ($Ring in $Rings) {
-    foreach ($Architecture in $Architectures) {
-        foreach ($Type in $Types) {
+foreach ($Ring in $Rings)
+{
+    foreach ($Architecture in $Architectures)
+    {
+        foreach ($Type in $Types)
+        {
 
-            $RegEx = "$Ring(?:.+\n)+((?:\d+\.)+\d+).+win-$Architecture.+(http.+\.$Type)"
+            $RegEx = "(?:$Ring \(R.+\)) (?:.+\n+)+((?:\d+\.)+\d+).+win-$Architecture.+(http.+\.$Type)"
 
-            if ($DownloadText -match $RegEx) {
-                New-NevergreenApp -Name 'Microsoft Teams' -Version $matches[1] -Uri $matches[2] -Architecture $Architecture -Type $Type -Ring $Ring
+            if ($DownloadText -match $RegEx)
+            {
+                New-NevergreenApp -Name 'Microsoft Teams' -Version $Matches[1] -Uri $Matches[2] -Architecture $Architecture -Type $Type -Ring $Ring
             }
-            else {
-                Write-Warning "No match found for Microsoft Teams $Channel $Architecture $Type"
+            else
+            {
+                Write-Warning "No match found for Microsoft Teams $Ring $Architecture $Type"
             }
 
         }
