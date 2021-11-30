64fbbc23397435ed1aa8f9c8eea6809ac2a82e40
Jonathan Pitre <jakeld@gmail.com>
2021-11-30 10:13:00 -0500

    Add msix release as suggested by Microsoft

--- Nevergreen/Apps/Get-MicrosoftTeams.ps1
+++ Nevergreen/Apps/Get-MicrosoftTeams.ps1
@@ -1,25 +1,44 @@
-$DownloadURL = 'https://raw.githubusercontent.com/ItzLevvie/MicrosoftTeams-msinternal/master/defconfig2'
+$DownloadURL = 'https://raw.githubusercontent.com/ItzLevvie/MicrosoftTeams-msinternal/master/defconfig'
 $DownloadText = (Invoke-WebRequest -Uri $DownloadURL -DisableKeepAlive -UseBasicParsing).Content
 
-$Rings = @('Canary', 'Teams', 'IT Admins Preview', 'Developers Partners Preview', 'Microsoft Dogfood', 'TAP/Beta Users', 'Public Developer Preview', 'Microsoft Outer Ring', 'Public')
+$Rings = @('Continuous Deployment', 'Exploration', 'Preview', 'Production')
 $Architectures = @('x64', 'x86', 'ARM64')
 $Types = @('Exe', 'Msi')
 
-foreach ($Ring in $Rings)
-{
-    foreach ($Architecture in $Architectures)
-    {
-        foreach ($Type in $Types)
-        {
+foreach ($Ring in $Rings) {
+    foreach ($Architecture in $Architectures) {
+        foreach ($Type in $Types) {
+
+            $RegEx = "(?:$Ring) (?:.+\n)+((?:\d+\.)+\d+).+win-$Architecture.+(http.+\.$Type)"
+
+            if ($DownloadText -match $RegEx) {
+                New-NevergreenApp -Name 'Microsoft Teams' -Version $Matches[1] -Uri $Matches[2] -Architecture $Architecture -Type $Type -Ring $Ring
+            }
+            else {
+                Write-Warning "No match found for Microsoft Teams $Ring $Architecture $Type"
+            }
+
+        }
+    }
+}
+
+$DownloadURL = 'https://raw.githubusercontent.com/ItzLevvie/MicrosoftTeams-msinternal/master/defconfig2'
+$DownloadText = (Invoke-WebRequest -Uri $DownloadURL -DisableKeepAlive -UseBasicParsing).Content
+
+$Rings = @('Public Developer Preview', 'Public')
+$Architectures = @('x64', 'x86', 'ARM64')
+$Types = @('Msix')
+
+foreach ($Ring in $Rings) {
+    foreach ($Architecture in $Architectures) {
+        foreach ($Type in $Types) {
 
             $RegEx = "(?:$Ring \(R.+\)) (?:.+\n)+((?:\d+\.)+\d+).+win-$Architecture.+(http.+\.$Type)"
 
-            if ($DownloadText -match $RegEx)
-            {
+            if ($DownloadText -match $RegEx) {
                 New-NevergreenApp -Name 'Microsoft Teams' -Version $Matches[1] -Uri $Matches[2] -Architecture $Architecture -Type $Type -Ring $Ring
             }
-            else
-            {
+            else {
                 Write-Warning "No match found for Microsoft Teams $Ring $Architecture $Type"
             }
 
