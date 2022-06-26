478730370769c98ac76841ffc5fc11b2e9a1cdb8
Dan Gough <goughdj@gmail.com>
2022-06-26 22:24:51 +0100

    Fix base URL

--- Nevergreen/Apps/Get-AdvancedIPScanner.ps1
+++ Nevergreen/Apps/Get-AdvancedIPScanner.ps1
@@ -1,4 +1,4 @@
-$URL32 = Get-Link -Uri 'https://www.advanced-ip-scanner.com' -MatchProperty href -Pattern 'Advanced_IP_Scanner.+\.exe'
+$URL32 = Get-Link -Uri 'https://www.advanced-ip-scanner.com/download' -MatchProperty href -Pattern 'Advanced_IP_Scanner.+\.exe'
 $Version = $URL32 | Get-Version
 
 New-NevergreenApp -Name 'Advanced IP Scanner' -Version $Version -Uri $URL32 -Architecture 'x86' -Type 'Exe'
