62c3b6bbe59a7bceac435fa2c37fee65c39b6384
Dan Gough <goughdj@gmail.com>
2022-06-26 22:18:10 +0100

    Fix download link handling

--- Nevergreen/Apps/Get-AutoIt.ps1
+++ Nevergreen/Apps/Get-AutoIt.ps1
@@ -1,5 +1,5 @@
 $Version = Get-Version -Uri 'https://www.autoitscript.com/site/autoit/downloads' -Pattern 'v((?:\d+\.)+\d+)'
 
-$URL32 = Get-Link -Uri 'https://www.autoitscript.com/site/autoit/downloads' -MatchProperty href -Pattern 'autoit-v.+-setup\.exe' -PrefixDomain
+$URL32 = (Get-Link -Uri 'https://www.autoitscript.com/site/autoit/downloads' -MatchProperty href -Pattern 'autoit-v.+-setup\.zip' -PrefixDomain | Resolve-Uri).Uri.Replace('.zip','.exe')
 
 New-NevergreenApp -Name 'AutoIt' -Version $Version -Uri $URL32 -Architecture 'x86' -Type 'Exe'
