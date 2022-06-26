ed72a711c003c087b3b13b307e530bee004b621c
Dan Gough <goughdj@gmail.com>
2022-06-26 21:19:08 +0100

    Replace OpenJDK v16 with v17

--- Nevergreen/Apps/Get-MicrosoftOpenJDK.ps1
+++ Nevergreen/Apps/Get-MicrosoftOpenJDK.ps1
@@ -1,7 +1,7 @@
-$URL11Zip,$URL11MSI,$URL16Zip,$URL16MSI = Get-Link -Uri 'https://docs.microsoft.com/en-gb/java/openjdk/download' -MatchProperty href -Pattern 'microsoft-jdk-11.+zip$','microsoft-jdk-11.+msi$','microsoft-jdk-16.+zip$','microsoft-jdk-16.+msi$'
-$Version11,$Version16 = $URL11MSI,$URL16MSI | Get-Version
+$URL11Zip,$URL11MSI,$URL17Zip,$URL17MSI = Get-Link -Uri 'https://docs.microsoft.com/en-gb/java/openjdk/download' -MatchProperty href -Pattern 'microsoft-jdk-11.+zip$','microsoft-jdk-11.+msi$','microsoft-jdk-17.+zip$','microsoft-jdk-17.+msi$'
+$Version11,$Version17 = $URL11MSI,$URL17MSI | Get-Version
 
 New-NevergreenApp -Name 'Microsoft OpenJDK' -Version $Version11 -Uri $URL11Zip -Architecture 'x64' -Type 'Zip'
 New-NevergreenApp -Name 'Microsoft OpenJDK' -Version $Version11 -Uri $URL11MSI -Architecture 'x64' -Type 'MSI'
-New-NevergreenApp -Name 'Microsoft OpenJDK' -Version $Version16 -Uri $URL16Zip -Architecture 'x64' -Type 'Zip'
-New-NevergreenApp -Name 'Microsoft OpenJDK' -Version $Version16 -Uri $URL16MSI -Architecture 'x64' -Type 'MSI'
+New-NevergreenApp -Name 'Microsoft OpenJDK' -Version $Version17 -Uri $URL17Zip -Architecture 'x64' -Type 'Zip'
+New-NevergreenApp -Name 'Microsoft OpenJDK' -Version $Version17 -Uri $URL17MSI -Architecture 'x64' -Type 'MSI'
