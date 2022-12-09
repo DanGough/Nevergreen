9650dd7be570ef66d72deb3df65092c8159cc50f
Dan Gough <goughdj@gmail.com>
2022-12-09 22:56:57 +0000

    Name corrected

--- Nevergreen/Apps/Get-OpenVPNCommunity.ps1
+++ Nevergreen/Apps/Get-OpenVPNCommunity.ps1
@@ -4,5 +4,5 @@ $Architectures = @('x64', 'x86', 'ARM64')
 
 foreach ($Architecture in $Architectures) {
         $URL = Get-Link -Uri 'https://openvpn.net/community-downloads/' -MatchProperty href -Pattern "$($Architecture.Replace('x64','amd64'))\.msi$"
-        New-NevergreenApp -Name 'OpenVPNConnect' -Version $Version -Uri $URL -Architecture $Architecture -Type 'Msi'
+        New-NevergreenApp -Name 'OpenVPNCommunity' -Version $Version -Uri $URL -Architecture $Architecture -Type 'Msi'
 }
