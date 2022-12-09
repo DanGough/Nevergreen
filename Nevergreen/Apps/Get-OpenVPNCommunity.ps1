4f290ff91fe40e635a1074d3588b3a62655e9361
Dan Gough <goughdj@gmail.com>
2022-12-09 23:03:12 +0000

    Pattern changed

--- Nevergreen/Apps/Get-OpenVPNCommunity.ps1
+++ Nevergreen/Apps/Get-OpenVPNCommunity.ps1
@@ -1,8 +1,12 @@
 $Version = Get-Version -Uri 'https://openvpn.net/community-downloads/' -Pattern 'OpenVPN ((?:\d+\.)+\d+\S*)'
 
-$Architectures = @('x64', 'x86', 'ARM64')
+$Releases = @(
+    @{Architecture = 'x86'; Type = 'Msi'; Pattern = 'x86\.msi$'}
+    @{Architecture = 'x64'; Type = 'Msi'; Pattern = 'amd64\.msi$'}
+    @{Architecture = 'ARM64'; Type = 'Msi'; Pattern = 'arm64\.msi$'}
+)
 
-foreach ($Architecture in $Architectures) {
-        $URL = Get-Link -Uri 'https://openvpn.net/community-downloads/' -MatchProperty href -Pattern "$($Architecture.Replace('x64','amd64'))\.msi$"
-        New-NevergreenApp -Name 'OpenVPNCommunity' -Version $Version -Uri $URL -Architecture $Architecture -Type 'Msi'
+foreach ($Release in $Releases) {
+        $URL = Get-Link -Uri 'https://openvpn.net/community-downloads/' -MatchProperty href -Pattern $Release.Pattern
+        New-NevergreenApp -Name 'OpenVPNCommunity' -Version $Version -Uri $URL -Architecture $Release.Architecture -Type $Release.Type
 }
