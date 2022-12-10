c25e2844e4dd545b79d678e4e5a3eed41ee1afec
Dan Gough <goughdj@gmail.com>
2022-12-10 18:24:53 +0000

    Add headers

--- Nevergreen/Apps/Get-AdobeDigitalEditions.ps1
+++ Nevergreen/Apps/Get-AdobeDigitalEditions.ps1
@@ -1,8 +1,8 @@
 try {
 
-    $Version = Get-Link -Uri 'https://www.adobe.com/uk/solutions/ebook/digital-editions/download.html' -MatchProperty href -Pattern '\.exe$' -ReturnProperty outerHTML | Get-Version -Pattern '>((?:\d+\.)+\d+)'
+    $Version = Get-Link -Uri 'https://www.adobe.com/uk/solutions/ebook/digital-editions/download.html' -Headers @{"accept-language"="en-GB,en;q=0.9,en-US;q=0.8"} -MatchProperty href -Pattern '\.exe$' -ReturnProperty outerHTML | Get-Version -Pattern '>((?:\d+\.)+\d+)'
 
-    $URL = Get-Link -Uri 'https://www.adobe.com/uk/solutions/ebook/digital-editions/download.html' -MatchProperty href -Pattern '\.exe$'
+    $URL = Get-Link -Uri 'https://www.adobe.com/uk/solutions/ebook/digital-editions/download.html' -Headers @{"accept-language"="en-GB,en;q=0.9,en-US;q=0.8"} -MatchProperty href -Pattern '\.exe$'
 
     New-NevergreenApp -Name 'Adobe Digital Editions' -Version $Version -Uri $URL -Architecture 'x86' -Type 'Exe'
 
