dd575b14b5ec58d89e87745c35728b609f45c024
Dan Gough <goughdj@gmail.com>
2022-12-10 23:35:12 +0000

    Add -ErrorAction Stop to try block

--- Nevergreen/Apps/Get-RizonesoftNotepad3.ps1
+++ Nevergreen/Apps/Get-RizonesoftNotepad3.ps1
@@ -6,7 +6,7 @@ $Releases = @(
 foreach ($Release in $Releases) {
     try {
         $URL = Get-Link -Uri 'https://www.rizonesoft.com/downloads/notepad3/' -MatchProperty outerHTML -Pattern $Release.Pattern -ReturnProperty href -ErrorAction Stop
-        $Version = Get-Version -Uri 'https://www.rizonesoft.com/downloads/notepad3/' -Pattern $Release.Pattern
+        $Version = Get-Version -Uri 'https://www.rizonesoft.com/downloads/notepad3/' -Pattern $Release.Pattern -ErrorAction Stop
         New-NevergreenApp -Name 'Notepad3' -Version $Version -Uri $URL -Architecture $Release.Architecture -Type $Release.Type
     }
     catch {
