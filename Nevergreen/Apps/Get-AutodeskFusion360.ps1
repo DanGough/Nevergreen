0988635e6f1d828c350768b2c4f1aa126686064a
Dan Gough <goughdj@gmail.com>
2023-03-23 21:36:48 +0000

    Removed in-progress app as added to Evergreen

--- Nevergreen/Apps/Get-AutodeskFusion360.ps1
+++
@@ -1,4 +0,0 @@
-$URL = Get-Link -Uri 'https://knowledge.autodesk.com/support/fusion-360/troubleshooting/caas/sfdcarticles/sfdcarticles/How-to-update-a-Lab-Installation-of-Fusion-360.html' -MatchProperty href -Pattern '\.exe$'
-$VersionURL = Resolve-Uri -Uri 'https://autode.sk/whatsnew' |  Select-Object -ExpandProperty Uri
-$Version = Get-Version -Uri $VersionURL -Pattern ">V\.?((?:\d+\.)+\d+)"
-New-NevergreenApp -Name 'Autodesk Fusion 360' -Version $Version -Uri $URL -Architecture 'x64' -Type 'Exe'
