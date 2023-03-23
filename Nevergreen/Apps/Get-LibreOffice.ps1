a7f8a34dffd4fa7f62f1760b75c752809245f387
Dan Gough <goughdj@gmail.com>
2023-03-23 22:17:22 +0000

    Fix LibreOffice

--- Nevergreen/Apps/Get-LibreOffice.ps1
+++ Nevergreen/Apps/Get-LibreOffice.ps1
@@ -15,8 +15,8 @@ foreach ($Branch in $Branches) {
     foreach ($Architecture in $Architectures) {
 
         try {
-            $DownloadPage = Get-Link -Uri "https://www.libreoffice.org/download/download-libreoffice/?type=win-$Architecture&version=$Version&lang=$Language" -MatchProperty href -Pattern "$Version.+$($Architecture.Replace('86_',''))\.msi$" -ErrorAction Stop
-            $URL = (Get-Link -Uri $DownloadPage -MatchProperty href -Pattern "$Version.+$($Architecture.Replace('86_',''))\.msi$" -ErrorAction Stop) -replace '^//','https://'
+            $DownloadPage = Get-Link -Uri "https://www.libreoffice.org/download/download-libreoffice/?type=win-$Architecture&version=$Version&lang=$Language" -MatchProperty href -Pattern "$Version[^`"]+$($Architecture.Substring($Architecture.Length-2,2))\.msi$" -ErrorAction Stop
+            $URL = (Get-Link -Uri $DownloadPage -MatchProperty href -Pattern "$Version[^`"]+$($Architecture.Substring($Architecture.Length-2,2))\.msi$" -ErrorAction Stop) -replace '^//','https://'
             New-NevergreenApp -Name 'LibreOffice' -Version $Version -Uri $URL -Architecture $Architecture.Replace('86_','') -Type 'msi' -Channel $Branch -Language 'Multi'
         }
         catch {
