056c12b0163a9da1053d922ddfd571d359389097
Dan Gough <goughdj@gmail.com>
2023-05-03 00:19:11 +0100

    Improved use of API results

--- Nevergreen/Apps/Get-GoogleDrive.ps1
+++ Nevergreen/Apps/Get-GoogleDrive.ps1
@@ -1,11 +1,10 @@
-Write-Warning "Google update API is unreliable and returns different values on each run."
-
 try {
-    #$Version = Get-Version -Uri 'https://support.google.com/a/answer/7577057?hl=en' -Pattern 'Version ((?:\d+\.)+\d+)'
-    $Version = (Invoke-RestMethod -Uri 'https://update.googleapis.com/service/update2' -Method Post -DisableKeepAlive -Body '<?xml version="1.0" encoding="UTF-8"?><request protocol="3.0" updaterversion="1.3.36.82"><app appid="{6BBAE539-2232-434A-A4E5-9A33560C6283}"><updatecheck/></app></request>').response.app.updatecheck.urls.url.codebase | Select-Object -First 1 | Get-Version
-    $URL = 'https://dl.google.com/drive-file-stream/GoogleDriveSetup.exe'
+    $Response = (Invoke-RestMethod -Uri 'https://update.googleapis.com/service/update2' -Method Post -DisableKeepAlive -Body '<?xml version="1.0" encoding="UTF-8"?><request protocol="3.0" updaterversion="1.3.36.82"><app appid="{6BBAE539-2232-434A-A4E5-9A33560C6283}"><updatecheck/></app></request>').response
+    $Version = $Response.app.updatecheck.manifest.version
+    $URL = @($Response.app.updatecheck.urls.url.codebase)[-1] + $Response.app.updatecheck.manifest.packages.package.name
+    $SHA256 = $Response.app.updatecheck.manifest.packages.package.hash_sha256
 
-    New-NevergreenApp -Name 'Google Drive' -Version $Version -Uri $URL -Architecture 'Multi' -Type 'Exe'
+    New-NevergreenApp -Name 'Google Drive' -Version $Version -Uri $URL -Architecture 'Multi' -Type 'Exe' -SHA256 $SHA256
 }
 catch {
     Write-Error "$($MyInvocation.MyCommand): $($_.Exception.Message)"
