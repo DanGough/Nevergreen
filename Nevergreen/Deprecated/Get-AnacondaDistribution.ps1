a9d268db9b330d64b82f0b261cb478b835aaae72
Dan Gough <goughdj@gmail.com>
2023-08-10 01:15:11 +0100

    Re-instate Anaconda/Miniconda

--- Nevergreen/Deprecated/Get-AnacondaDistribution.ps1
+++
@@ -1,3 +0,0 @@
-$URL = Get-Link -Uri 'https://www.anaconda.com/products/individual' -MatchProperty href -Pattern '\.exe$'
-$Version = $URL | Get-Version
-New-NevergreenApp -Name 'AnacondaDistribution' -Version $Version -Uri $URL -Architecture 'x64' -Type 'exe'
