37252d7fcb04226efa600d393946d09d599ccd8e
Dan Gough <goughdj@gmail.com>
2023-09-06 22:54:31 +0100

    URLs now fully resolved

--- Nevergreen/Apps/Get-MicrosoftSSMS.ps1
+++ Nevergreen/Apps/Get-MicrosoftSSMS.ps1
@@ -1,4 +1,4 @@
-$URL64 = 'https://aka.ms/ssmsfullsetup'
+$URL64 = (Resolve-Uri -Uri 'https://aka.ms/ssmsfullsetup').Uri
 
 $Version = Get-Version -Uri 'https://docs.microsoft.com/en-us/sql/ssms/download-sql-server-management-studio-ssms' -Pattern 'Release number: ((?:\d+\.)+\d+)'
 
