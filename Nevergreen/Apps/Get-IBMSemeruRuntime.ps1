55e285d3fd0265bb80a91cf655d4e04a054bfb51
Dan Gough <goughdj@gmail.com>
2023-09-07 10:25:19 +0100

    Fixed

--- Nevergreen/Apps/Get-IBMSemeruRuntime.ps1
+++ Nevergreen/Apps/Get-IBMSemeruRuntime.ps1
@@ -1,11 +1,12 @@
-$DownloadPage = (Invoke-WebRequest -Uri 'https://developer.ibm.com/languages/java/semeru-runtimes/downloads' -DisableKeepAlive).Content
+#$DownloadPage = (Invoke-WebRequest -Uri 'https://developer.ibm.com/languages/java/semeru-runtimes/downloads' -DisableKeepAlive).Content
+$DownloadPage = (Invoke-RestMethod -Uri 'https://developer.ibm.com/middleware/v1/contents/static/semeru-runtime-downloads' -DisableKeepAlive).results.pagepost_custom_js_value
 
 if ($DownloadPage -match 'let sourceDataJson = ([^;]+);') {
     $Downloads = (ConvertFrom-Json $matches[1]).Downloads | Where-Object os -EQ 'Windows'
 }
 
 foreach ($Download in $Downloads) {
-    $Version = $Download.name | Get-Version
+    $Version = ($Download.name.Replace('+','.') -Replace '.+OpenJDK (.+)','$1') | Get-Version
     New-NevergreenApp -Name "IBM Semeru Runtime JDK" -Version $Version -Uri $Download.jdk.opt1.downloadlink -Architecture $Download.arch -Type (Get-Culture).TextInfo.ToTitleCase($Download.jdk.opt1.displayName)
     New-NevergreenApp -Name "IBM Semeru Runtime JDK" -Version $Version -Uri $Download.jdk.opt2.downloadlink -Architecture $Download.arch -Type (Get-Culture).TextInfo.ToTitleCase($Download.jdk.opt2.displayName)
     New-NevergreenApp -Name "IBM Semeru Runtime JRE" -Version $Version -Uri $Download.jre.opt1.downloadlink -Architecture $Download.arch -Type (Get-Culture).TextInfo.ToTitleCase($Download.jre.opt1.displayName)
