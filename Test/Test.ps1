54444574787989febc669d3499704124b6a9c820
Dan Gough <goughdj@gmail.com>
2023-11-27 12:16:21 +0000

    Cleaned up

--- Test/Test.ps1
+++ Test/Test.ps1
@@ -1,6 +1,21 @@
 . "$PSScriptRoot\ImportFunctions.ps1"
+
 Start-Transcript -Path "$env:temp\Nevergreen.log" -Force
 Clear-Host
 Write-Host "PowerShell version: $($PSVersionTable.PSVersion)"
-Find-NevergreenApp | Get-NevergreenApp
+$Apps = Find-NevergreenApp
+
+foreach ($App in $Apps) {
+    Write-Host "$App`:"
+    try {
+        $Result = Get-NevergreenApp $App
+        $Result | Format-List
+        #$Result.URI | ForEach-Object {
+            #Invoke-Download $_
+        #}
+    }
+    catch {
+        Write-Error $_
+    }
+}
 Stop-Transcript
