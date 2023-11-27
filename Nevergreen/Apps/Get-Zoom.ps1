b1716ffdd6a89d34903a0abde9b88654fb0a2142
Dan Gough <goughdj@gmail.com>
2023-11-27 12:15:50 +0000

    Updated regex

--- Nevergreen/Apps/Get-Zoom.ps1
+++ Nevergreen/Apps/Get-Zoom.ps1
@@ -1,11 +1,11 @@
-$VDILinks = (Invoke-WebRequest -Uri 'https://support.zoom.us/hc/en-us/articles/4415057249549' -UseBasicParsing -UserAgent 'Microsoft URL Control - 6.01.9782').Links
+$VDILinks = (Invoke-WebRequest -Uri 'https://support.zoom.us/hc/en-us/articles/4415057249549' -UseBasicParsing -UserAgent 'Googlebot/2.1 (+http://www.google.com/bot.html)').Links
 
 $Apps = @(
     @{Name = 'Zoom VDI Client'; Architecture = 'x86'; Type = 'Msi'; Pattern = 'ZoomInstallerVDI.+?msi$' }
-    @{Name = 'Zoom VDI Client'; Architecture = 'x64'; Type = 'Msi'; Pattern = 'ZoomInstallerVDI.+?msi\?archType=x64$' }
+    @{Name = 'Zoom VDI Client'; Architecture = 'x64'; Type = 'Msi'; Pattern = 'ZoomInstallerVDI.+?msi\?archType.+x64$' }
     @{Name = 'Zoom Citrix Plugin'; Architecture = 'x86'; Type = 'Msi'; Pattern = 'ZoomCitrixHDXMediaPlugin\.msi$' }
     @{Name = 'Zoom VDI Universal Plugin'; Architecture = 'x86'; Type = 'Msi'; Pattern = 'ZoomVDIUniversalPlugin.+?msi$' }
-    @{Name = 'Zoom VDI Universal Plugin'; Architecture = 'x64'; Type = 'Msi'; Pattern = 'ZoomVDIUniversalPlugin.+?msi\?archType=x64$' }
+    @{Name = 'Zoom VDI Universal Plugin'; Architecture = 'x64'; Type = 'Msi'; Pattern = 'ZoomVDIUniversalPlugin.+?msi\?archType.+x64$' }
 )
 
 foreach ($App in $Apps) {
@@ -16,6 +16,6 @@ foreach ($App in $Apps) {
         New-NevergreenApp -Name $App.Name -Version $Version -Uri $URL -Architecture $App.Architecture -Type $App.Type
     }
     else {
-        Write-Warning -Message "Unable to retrieve details for $($App.Name)"
+        Write-Warning -Message "Unable to retrieve details for $($App.Name) $($App.Architecture) $($App.Type)"
     }
 }
