fce45b4faac1b5fa1a1a9315cdfbdc4acc5f4efd
Dan Gough <goughdj@gmail.com>
2023-11-27 12:13:00 +0000

    Now returns 4-part version number

--- Nevergreen/Apps/Get-8x8Work.ps1
+++ Nevergreen/Apps/Get-8x8Work.ps1
@@ -1,5 +1,5 @@
 $URL = Get-Link -Uri 'https://support.8x8.com/cloud-phone-service/voice/work-desktop/download-8x8-work-for-desktop' -MatchProperty href -Pattern '\.msi'
 
-$Version = $URL | Get-Version -Pattern '((?:\d+\.)+\d+)'
+$Version = $URL | Get-Version -ReplaceWithDot
 
 New-NevergreenApp -Name '8x8 Work' -Version $Version -Uri $URL -Architecture 'x64' -Type 'msi'
