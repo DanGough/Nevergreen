3af206140da9b4f039e1f7bcf8f9f9fc4b562623
codaamok <github@adamcook.io>
2024-02-16 09:52:24 +0000

    Adds argument completer for public functions

--- Nevergreen/Nevergreen.psm1
+++ Nevergreen/Nevergreen.psm1
@@ -11,3 +11,12 @@ ForEach ($Import in @($Public + $Private)) {
 }
 
 Export-ModuleMember -Function $Public.Basename
+
+Register-ArgumentCompleter -CommandName 'Get-NevergreenApp', 'Find-NevergreenApp' -ParameterName 'Name' -ScriptBlock {
+    param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameters)
+
+    $Apps = Get-ChildItem -Path ([System.IO.Path]::Combine($PSScriptRoot, 'Apps', '*.ps1'))
+    $Apps.Name -replace '^Get-' -replace '\.ps1$' | Where-Object {
+        $_ -like "$wordToComplete*"
+    }
+}
